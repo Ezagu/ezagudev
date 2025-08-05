9e536335c3b3dbf26b2d1ce6fc2122364b377da1
Ezagu <agustincalpedm@gmail.com>
2025-08-05 16:24:07 -0300

    html, css of project page

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -4,7 +4,10 @@ import './Footer.css';
 export const Footer = () => {
   return (
     <footer className='footer'>
-      <p>&copy; {new Date().getFullYear()} - Hecho con 💖 por Ezagu - <a href="https://github.com/Ezagu" target="_blank" rel="noreferrer">github page</a></p>
+      <p>
+        &copy; {new Date().getFullYear()} - Hecho con 💖 por Ezagu - 
+        <a href="https://github.com/Ezagu/ezagudev" target="_blank" rel="noreferrer">github page</a>
+      </p>
     </footer>
   )
 }
--- src/pages/Projects/Projects.js
+++ src/pages/Projects/Projects.js
@@ -1,7 +1,74 @@
 import React from 'react'
+import './Projects.css'
 
 export const Projects = () => {
   return (
-    <div>Projects</div>
+    <div className='project-page'>
+      <h1 className='page-title'>Projects</h1>
+      <p className='page-desc'>Algunos de los proyectos que realicé</p>
+      <div className='filter'>
+        <button className='filter-item filter-item-active'>Javascript</button>
+        <button className='filter-item'>Html</button>
+        <button className='filter-item'>Css</button>
+        <button className='filter-item'>React</button>
+      </div>
+      <section className='projects'>
+        <article className='project'>
+          <div className='project-img'>
+            <img src='/images/projects/mechanography.png'/>
+          </div>
+          <h3 className='project-title'>Mechanography</h3>
+          <p className='project-desc'>Test de cuan rápido escribes</p>
+          <div className='project-tec'>
+            <div className='tec'>html</div>
+            <div className='tec'>css</div>
+            <div className='tec'>javascript</div>
+          </div>
+          <button className='button project-button'>Conoce más &gt;</button>
+        </article>
+
+        <article className='project'>
+          <div className='project-img'>
+            <img src='/images/projects/mechanography.png'/>
+          </div>
+          <h3 className='project-title'>Mechanography</h3>
+          <p className='project-desc'>Test de cuan rápido escribes</p>
+          <div className='project-tec'>
+            <div className='tec'>html</div>
+            <div className='tec'>css</div>
+            <div className='tec'>javascript</div>
+          </div>
+          <button className='button project-button'>Conoce más &gt;</button>
+        </article>
+
+        <article className='project'>
+          <div className='project-img'>
+            <img src='/images/projects/mechanography.png'/>
+          </div>
+          <h3 className='project-title'>Mechanography</h3>
+          <p className='project-desc'>Test de cuan rápido escribes</p>
+          <div className='project-tec'>
+            <div className='tec'>html</div>
+            <div className='tec'>css</div>
+            <div className='tec'>javascript</div>
+          </div>
+          <button className='button project-button'>Conoce más &gt;</button>
+        </article>
+
+        <article className='project'>
+          <div className='project-img'>
+            <img src='/images/projects/mechanography.png'/>
+          </div>
+          <h3 className='project-title'>Mechanography</h3>
+          <p className='project-desc'>Test de cuan rápido escribes</p>
+          <div className='project-tec'>
+            <div className='tec'>html</div>
+            <div className='tec'>css</div>
+            <div className='tec'>javascript</div>
+          </div>
+          <button className='button project-button'>Conoce más &gt;</button>
+        </article>
+      </section>
+    </div>
   )
 }
