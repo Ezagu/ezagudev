d484a6b24e47c6add82cbef04c8e71131bfd6c9a
Ezagu <agustincalpedm@gmail.com>
2025-08-05 17:09:42 -0300

    html and css of the skills page

--- src/pages/Skills/Skills.js
+++ src/pages/Skills/Skills.js
@@ -1,7 +1,74 @@
 import React from 'react'
+import './Skills.css'
 
 export const Skills = () => {
   return (
-    <div>Skills</div>
+    <div className='skill-page'>
+      <h1 className='page-title'>Skills</h1>
+      <p className='page-desc'>Las habilidades, herramientas y tecnologías en las que soy bueno</p>
+      <section className='skill-content'>
+        <article className='skill-area'>
+          <h2 className='area-title'>Frontend</h2>
+          <div className='skill-container'>
+            <div className='skill'>
+              <img className='skill-icon' src='images/skills/html.svg'/>
+              <h3 className='skill-name'>HTML</h3>
+            </div>
+            <div className='skill'>
+              <img className='skill-icon'/>
+              <h3 className='skill-name'>HTML</h3>
+            </div>
+            <div className='skill'>
+              <img className='skill-icon'/>
+              <h3 className='skill-name'>HTML</h3>
+            </div>
+            <div className='skill'>
+              <img className='skill-icon'/>
+              <h3 className='skill-name'>HTML</h3>
+            </div>
+            <div className='skill'>
+              <img className='skill-icon'/>
+              <h3 className='skill-name'>HTML</h3>
+            </div>
+          </div>
+        </article>
+
+        <article className='skill-area'>
+          <h2 className='area-title'>Backend</h2>
+          <div className='skill-container'>
+            <div className='skill'>
+              <img className='skill-icon' src='images/skills/html.svg'/>
+              <h3 className='skill-name'>HTML</h3>
+            </div>
+            <div className='skill'>
+              <img className='skill-icon'/>
+              <h3 className='skill-name'>HTML</h3>
+            </div>
+            <div className='skill'>
+              <img className='skill-icon'/>
+              <h3 className='skill-name'>HTML</h3>
+            </div>
+          </div>
+        </article>
+
+        <article className='skill-area'>
+          <h2 className='area-title'>Herramientas</h2>
+          <div className='skill-container'>
+            <div className='skill'>
+              <img className='skill-icon' src='images/skills/html.svg'/>
+              <h3 className='skill-name'>HTML</h3>
+            </div>
+            <div className='skill'>
+              <img className='skill-icon'/>
+              <h3 className='skill-name'>HTML</h3>
+            </div>
+            <div className='skill'>
+              <img className='skill-icon'/>
+              <h3 className='skill-name'>HTML</h3>
+            </div>
+          </div>
+        </article>
+      </section>
+    </div>
   )
 }
