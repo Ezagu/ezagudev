786944718b0cebaf2012d9068ff1f07c40b7fd02
Ezagu <agustincalpedm@gmail.com>
2025-08-05 18:53:37 -0300

    html and css of contact page

--- src/pages/Contact/Contact.js
+++ src/pages/Contact/Contact.js
@@ -1,7 +1,57 @@
 import React from 'react'
+import './Contact.css'
 
 export const Contact = () => {
   return (
-    <div>Contact</div>
+    <div className='contact-page'>
+      <h1 className='page-title'>Contact</h1>
+      <p className='page-desc'>Querés trabajar conmigo? Estoy abierto a nuevas oportunidades laborales o colaboraciones. No dudes en escribirme!</p>
+
+      <div className='contact-content'>
+        <div className='info-contact'>
+          <h2 className='area-title'>Información</h2>
+          <h3 className='item-title'>
+            Email
+          </h3>
+          <div className='info-item'>
+            agustincalpedm@gmail.com
+          </div>
+          <h3 className='item-title'>
+            Telefono
+          </h3>
+          <div className='info-item'>
+            +54 11 2587-5034
+          </div>
+          <h3 className='item-title'>
+            Ubicacion
+          </h3>
+          <div className='info-item'>
+            Merlo, Buenos Aires, Argentina
+          </div>
+          <button className='button info-button'>
+            <a href='https://www.github.com/Ezagu' target='_blank' rel='noreferrer'>
+              Github &gt;
+            </a>
+            </button>
+          <button className='button info-button'>
+            <a href='https://www.linkedin.com/in/agustin-calpe/' target='_blank' rel='noreferrer'>
+              Linkedin &gt;
+            </a>
+          </button>
+        </div>
+        <form className='form'>
+          <h2 className='area-title'>Envíame un mensaje</h2>
+          <h3 className='item-title'>Nombre</h3>
+          <input type='text'/>
+          <h3 className='item-title'>Email</h3>
+          <input type='email'/>
+          <h3 className='item-title'>Asunto</h3>
+          <input type='text'/>
+          <h3 className='item-title'>Mensaje</h3>
+          <textarea></textarea>
+          <input type='submit' value='ENVIAR' className='button form-submit'/>
+        </form>
+      </div>
+    </div>
   )
 }
