bb175c6587fd077b6c48529318f84296bf79ca2c
Ezagu <agustincalpedm@gmail.com>
2025-08-05 00:56:19 -0300

    Improve css layout

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,13 +1,18 @@
 import React from 'react';
-import { NavLink } from 'react-router';
+import { NavLink, Link } from 'react-router';
 import './Header.css';
 
 export const Header = () => {
   return (
     <header className='header'>
-      <h2 className='header-title'> &lt;Ezagu /&gt; </h2>
+      <h2 className='header-title'><Link to='/home'>&lt;Ezagu /&gt;</Link></h2>
       <nav>
         <ul className='nav-list'>
+          <li>
+            <button className='nav-button'>
+              <NavLink to='/home'>Home</NavLink>
+            </button>
+          </li>
           <li>
             <button className='nav-button'>
               <NavLink to='/projects'>Projects</NavLink>
