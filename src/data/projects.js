d058e250f99f09a63acd4fd5acc76789e884dd4d
Ezagu <agustincalpedm@gmail.com>
2025-08-05 20:33:40 -0300

    create projects data and load it in projects page

---
+++ src/data/projects.js
@@ -0,0 +1,68 @@
+const projects = [
+  {
+    id: 'mechanography',
+    nombre: 'Mechanography',
+    img: 'images/projects/mechanography.webp',
+    desc_short: '',
+    desc: '',
+    tecs: [
+      'html',
+      'css',
+      'javascript'
+    ],
+    git: 'https://www.github.com/Ezagu/Mechanography',
+    url: 'https://mechanography.vercel.app/'
+  }, {
+    id: 'todolist',
+    nombre: 'To-Do List',
+    img: 'images/projects/todolist.webp',
+    desc_short: '',
+    desc: '',
+    tecs: [
+      'html',
+      'css',
+      'javascript'
+    ],
+    git: 'https://github.com/Ezagu/ToDoList',
+    url: 'https://to-do-list-beta-six-99.vercel.app/'
+  }, {
+    id: 'weatherapp',
+    nombre: 'Weather app',
+    img: 'images/projects/wheaterapp.webp',
+    desc_short: '',
+    desc: '',
+    tecs: [
+      'html',
+      'css',
+      'javascript'
+    ],
+    git: 'https://github.com/Ezagu/WeatherApp',
+    url: 'https://weather-app-theta-lemon-67.vercel.app/'
+  }, {
+    id: 'minesweeper',
+    nombre: 'Minesweeper',
+    img: 'images/projects/minesweeper.webp',
+    desc_short: '',
+    desc: '',
+    tecs: [
+      'python',
+      'tkinter'
+    ],
+    git: 'https://github.com/Ezagu/Minesweeper',
+    url: undefined
+  }, {
+    id: 'calculadora',
+    nombre: 'Calculadora',
+    img: 'images/projects/calculadora.webp',
+    desc_short: '',
+    desc: '',
+    tecs: [
+      'python',
+      'tkinter'
+    ],
+    git: 'https://github.com/Ezagu/CalculadoraPython',
+    url: undefined
+  }
+];
+
+export default projects;
--- src/pages/Projects/Projects.js
+++ src/pages/Projects/Projects.js
@@ -1,5 +1,8 @@
-import React from 'react'
-import './Projects.css'
+import React from 'react';
+import './Projects.css';
+import projects from '../../data/projects.js';
+
+console.log(projects);
 
 export const Projects = () => {
   return (
@@ -13,61 +16,25 @@ export const Projects = () => {
         <button className='filter-item'>React</button>
       </div>
       <section className='projects'>
-        <article className='project'>
-          <div className='project-img'>
-            <img src='/images/projects/mechanography.png' alt=''/>
-          </div>
-          <h3 className='project-title'>Mechanography</h3>
-          <p className='project-desc'>Test de cuan rápido escribes</p>
-          <div className='project-tec'>
-            <div className='tec'>html</div>
-            <div className='tec'>css</div>
-            <div className='tec'>javascript</div>
-          </div>
-          <button className='button project-button'>Conoce más &gt;</button>
-        </article>
-
-        <article className='project'>
-          <div className='project-img'>
-            <img src='/images/projects/mechanography.png' alt=''/>
-          </div>
-          <h3 className='project-title'>Mechanography</h3>
-          <p className='project-desc'>Test de cuan rápido escribes</p>
-          <div className='project-tec'>
-            <div className='tec'>html</div>
-            <div className='tec'>css</div>
-            <div className='tec'>javascript</div>
-          </div>
-          <button className='button project-button'>Conoce más &gt;</button>
-        </article>
-
-        <article className='project'>
-          <div className='project-img'>
-            <img src='/images/projects/mechanography.png' alt=''/>
-          </div>
-          <h3 className='project-title'>Mechanography</h3>
-          <p className='project-desc'>Test de cuan rápido escribes</p>
-          <div className='project-tec'>
-            <div className='tec'>html</div>
-            <div className='tec'>css</div>
-            <div className='tec'>javascript</div>
-          </div>
-          <button className='button project-button'>Conoce más &gt;</button>
-        </article>
-
-        <article className='project'>
-          <div className='project-img'>
-            <img src='/images/projects/mechanography.png' alt=''/>
-          </div>
-          <h3 className='project-title'>Mechanography</h3>
-          <p className='project-desc'>Test de cuan rápido escribes</p>
-          <div className='project-tec'>
-            <div className='tec'>html</div>
-            <div className='tec'>css</div>
-            <div className='tec'>javascript</div>
-          </div>
-          <button className='button project-button'>Conoce más &gt;</button>
-        </article>
+        {
+          projects.map((project, i) => (
+            <article className='project' key={i}>
+              <div className='project-img'>
+                <img src={project.img} alt={project.nombre + 'screenshot'}/>
+              </div>
+              <h3 className='project-title'>{project.nombre}</h3>
+              <p className='project-desc'>{project.desc_short}</p>
+              <div className='project-tec'>
+                {
+                  project.tecs.map((tec) => (
+                    <div className='tec'>{tec}</div>
+                  ))
+                }
+              </div>
+              <button className='button project-button'>Conoce más &gt;</button>
+            </article>
+          ))
+        }
       </section>
     </div>
   )
